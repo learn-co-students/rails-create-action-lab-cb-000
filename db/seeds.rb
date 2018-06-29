594e824524abf1a233f69da8c948479118eb58c6
Alex Segers <a.segers.dev@gmail.com>
2018-06-29 20:50:21 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def seed_students(names_arr)
+  names_arr.each do |name|
+    @student = Student.new
+    @student.first_name = name.split(" ").first
+    @student.last_name = name.split(" ").last
+    @student.save
+  end
+
+  Student.count == names_arr.count ? "Created #{Student.count} Students" : "Created #{Student.count} Students, Indexes #{++Student.count} Through #{Student.count + names_arr.count}"
+end
+
+NAMES = ["Archie Andrews", "Veronica Lodge", "Cheryl Blossom", "Jughead Jones", "Betty Cooper"]
+
+seed_students(NAMES)
