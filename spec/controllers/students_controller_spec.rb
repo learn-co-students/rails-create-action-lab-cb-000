517166ffd822488ab5e632b9de3cb72db26d4939
Laura Felce <crepu83@yahoo.it>
2019-05-23 09:03:12 +0000

    Done.

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
-#describe StudentsController do
+describe StudentsController do
 
-  #describe "POST create" do
-    #context "with valid attributes" do
-      #it "creates a new student" do
-        #expect{
-          #post :create, { :first_name => "Sam", :last_name => "Smith" }
-        #}.to change(Student,:count).by(1)
-      #end
+  describe "POST create" do
+    context "with valid attributes" do
+      it "creates a new student" do
+        expect{
+          post :create, { :first_name => "Sam", :last_name => "Smith" }
+        }.to change(Student,:count).by(1)
+      end
 
-      #it "redirects to the new student" do
-        #post :create, { :first_name => "Sam", :last_name => "Smith" }
-        #expect(response).to redirect_to Student.last
-      #end
-    #end
-  #end
+      it "redirects to the new student" do
+        post :create, { :first_name => "Sam", :last_name => "Smith" }
+        expect(response).to redirect_to Student.last
+      end
+    end
+  end
 
-#end
-
-#Lab was done correctly but there was a issue with the specs:  ThreadError: already initialized. May be possibly due to the Rails version.
+end
